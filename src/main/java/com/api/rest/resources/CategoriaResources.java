b8bb53cfa4f38947264a7ac47987908947d25fdb
Gabriel Matheus <GabrielMth>
2024-08-22 22:40:36 -0300

    Criado GET por ID na path e Post retornando location manual apesar que o .created já retorna de forma automatica. (case de estudo e curiosidade)

--- src/main/java/com/api/rest/resources/CategoriaResources.java
+++ src/main/java/com/api/rest/resources/CategoriaResources.java
@@ -2,13 +2,16 @@ package com.api.rest.resources;
 
 import com.api.rest.model.Categoria;
 import com.api.rest.repository.CategoriaRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigureOrder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 //META DADOS. - ANOTAÇÕES.
 @RestController
@@ -20,6 +23,32 @@ public class CategoriaResources {
 
     @GetMapping
     public List<Categoria> listar() {
+        // public ResponseEntity<?> listar() {
+        // List<Categoria> categorias = categoriaRepository.findAll();         // não fica bom mas é possível trocar de acordo com um estado da list.
+        // return !categorias.isEmpty() ? ResponseEntity.ok(categorias) : ResponseEntity.noContent().build();
         return categoriaRepository.findAll();
     }
+
+    @PostMapping
+    public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+        Categoria categoriaSalva = categoriaRepository.save(categoria);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(categoriaSalva.getCodigo()).toUri();
+
+        response.setHeader("Location", uri.toASCIIString());
+
+        return ResponseEntity.created(uri).body(categoriaSalva);
+    }
+
+    @GetMapping("/{codigo}")
+    public ResponseEntity<Categoria> buscarPeloId(@PathVariable Long codigo) {
+        Optional<Categoria> categoria = categoriaRepository.findById(codigo);
+
+        if (!categoria.isEmpty()) {
+            return ResponseEntity.ok(categoria.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 }
